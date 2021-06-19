71a795b62cc49a43a102b8ef47cade4346a77fd9
masuda0421 <82562239+masuda0421@users.noreply.github.com>
2021-06-20 02:34:06 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -21,38 +21,17 @@ function start () {
     me = game.createSprite(2, 4)
     count = 0
 }
+let 配列 = 0
+let ana = 0
 let kabe4: game.LedSprite = null
 let kabe3: game.LedSprite = null
 let kabe2: game.LedSprite = null
 let kabe1: game.LedSprite = null
-let 配列 = 0
-let ana = 0
 let count = 0
 let kabes: number[] = []
 let _null = 0
 let me: game.LedSprite = null
 start()
-// 壁が迫ってくる
-basic.forever(function () {
-    _null = 1
-    if (_null == 1) {
-        ana = randint(0, 4)
-        配列 = kabes.removeAt(ana)
-        for (let index = 0; index <= 5; index++) {
-            kabe1 = game.createSprite(kabes[0], index)
-            kabe2 = game.createSprite(kabes[1], index)
-            kabe3 = game.createSprite(kabes[2], index)
-            kabe4 = game.createSprite(kabes[3], index)
-            basic.pause(500)
-            kabe1.delete()
-            kabe2.delete()
-            kabe3.delete()
-            kabe4.delete()
-        }
-        kabes.push(配列)
-        count += 1
-    }
-})
 // 壁に当たってしまうとゲームオーバーになる。
 // 通り抜けた壁の数で点数が決まり、点数によって表示される表情が変化する。
 // 
@@ -76,5 +55,26 @@ basic.forever(function () {
             game.gameOver()
             basic.clearScreen()
         }
+    }
+})
+// 壁が迫ってくる
+basic.forever(function () {
+    _null = 1
+    if (_null == 1) {
+        ana = randint(0, 4)
+        配列 = kabes.removeAt(ana)
+        for (let index = 0; index <= 4; index++) {
+            kabe1 = game.createSprite(kabes[0], index)
+            kabe2 = game.createSprite(kabes[1], index)
+            kabe3 = game.createSprite(kabes[2], index)
+            kabe4 = game.createSprite(kabes[3], index)
+            basic.pause(500)
+            kabe1.delete()
+            kabe2.delete()
+            kabe3.delete()
+            kabe4.delete()
+        }
+        kabes.push(配列)
+        count += 1
     }
 })
